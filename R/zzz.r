27a037d3c22029b7020f5d70a78135e8c60355ed
Jeremy Silver <jeremy.silver@unimelb.edu.au>
2021-02-16 09:29:46 +1100

    Cleaned up package name (the old name was still use in places)

--- R/zzz.r
+++ R/zzz.r
@@ -1,38 +1,39 @@
 .First.lib <- function(lib, pkg) {
-##   library.dynam("EmulatorProject", pkg, lib)
-##   packageStartupMessage("Use help(EmulatorProject) for an overview of this library")
+##   library.dynam("EmulatorAnalysis", pkg, lib)
+##   packageStartupMessage("Use help(EmulatorAnalysis) for an overview of this library")
   cat('Running .First.lib \n')
 }
 
 .Last.lib <- function(lib, pkg) {
-##   library.dynam("EmulatorProject", pkg, lib)
-##   packageStartupMessage("Use help(EmulatorProject) for an overview of this library")
+##   library.dynam("EmulatorAnalysis", pkg, lib)
+##   packageStartupMessage("Use help(EmulatorAnalysis) for an overview of this library")
   cat('Running .Last.lib \n')
 }
 
 ## .onLoad <- function(lib, pkg) {
-##   library.dynam("EmulatorProject", pkg, lib)
-##   packageStartupMessage("Use help(EmulatorProject) for an overview of this library")
+##   library.dynam("EmulatorAnalysis", pkg, lib)
+##   packageStartupMessage("Use help(EmulatorAnalysis) for an overview of this library")
 ##   cat('Running onLoad \n')
 ## }
 
 .onUnload <- function(lib, pkg) {
-##   library.dynam("EmulatorProject", pkg, lib)
-##   packageStartupMessage("Use help(EmulatorProject) for an overview of this library")
+##   library.dynam("EmulatorAnalysis", pkg, lib)
+##   packageStartupMessage("Use help(EmulatorAnalysis) for an overview of this library")
   cat('Running onLoad \n')
 }
 
 .onAttach <- function(lib, pkg) {
-  library.dynam("EmulatorProject", pkg, lib)
-  packageStartupMessage("Use help(EmulatorProject) for an overview of this library")
-  cat('Running onAttach \n')
+    print(c(pkg,lib))
+    # library.dynam("EmulatorAnalysis", pkg, lib)
+    packageStartupMessage("Use help(EmulatorAnalysis) for an overview of this library")
+    cat('Running onAttach \n')
 }
 
 ## .onLoad<-function(libname, pkgname){
-##   library.dynam("EmulatorProject", pkg, lib)
-##   packageStartupMessage("Use help(EmulatorProject) for an overview of this library")
+##   library.dynam("EmulatorAnalysis", pkg, lib)
+##   packageStartupMessage("Use help(EmulatorAnalysis) for an overview of this library")
 ## }
 
 ## .onUnload<-function(libpath){
-##   library.dynam.unload('EmulatorProject', libpath)
+##   library.dynam.unload('EmulatorAnalysis', libpath)
 ## }
